72bd5f7aa728419831096d7ca50a7d807c87bb52
sheep333 <yukiko.elike1.51@gmail.com>
2020-07-23 22:34:22 +0900

    コマンドクラスを修正

--- command.py
+++ command.py
@@ -1,27 +1,34 @@
-import pandas as pd
+import json
 import logging
+import numpy as np
+from os import getenv
+import pandas as pd
+from redminelib import Redmine
+from redminelib.exceptions import ResourceAttrError
 
-from .redmine import RedmineModule
-from .git import GitChecker
+from git import GitChecker
 
 logger = logging.getLogger(__name__)
 
 
 class Command():
-    command_list = ['merge_check', 'get_issues']
+    command_list = ["merge_check", "get_issues", "check_user_time"]
 
     def __init__(self):
-        self.data = pd.read_json('json/settings.json')
-        self.redmine = RedmineModule(**self.data['redmine_auth'])
+        tmp = open("json/settings.json")
+        self.data = json.load(tmp)
+        self.redmine = Redmine(self.data["redmine_url"], key=getenv("KEY"))
 
     @classmethod
     def execute_command(cls, command):
-        if not cls._check_command(command):
-            raise ValueError('存在しないコマンドです')
-        elif command == 'merge_check':
+        if not cls._check_command(cls, command):
+            raise ValueError(f"{command}は存在しないコマンドです")
+        elif command == "merge_check":
             cls._merge_check()
-        elif command == 'get_issues':
+        elif command == "get_issues":
             cls._get_issues()
+        elif command == "check_user_time":
+            cls._check_user_time()
 
     def _check_command(self, command):
         if command in self.command_list:
@@ -30,26 +37,50 @@ class Command():
 
     def _merge_check(self):
         # RedmineのIssueフィルターをマージしてIssueを検索
-        issues = self.redmine.filter_issues(**self.data['issue_filter'])
+        issues = self.redmine.filter_issues(**self.data["merge_check_filter"])
 
         # Githubのマージされたブランチと比較
         result = []
-        git_checker = GitChecker(**self.data['git_data'])
+        git_checker = GitChecker(**self.data["git_data"])
         for issue in issues:
             result.append(git_checker.merge_check(issue.id))
-        logger.info(f'Check of git succeeded!!')
+        logger.info(f"Check of git succeeded!!")
 
         # CSV化する
-        logger.info('Create CSV')
-        df = pd.DataFrame(result, columns=['issue_id', 'output'])
+        logger.info("Create CSV")
+        df = pd.DataFrame(result, columns=["issue_id", "output"])
         df.to_csv(f"./output/merge_check_{self.data['git_data']['branch_name']}.csv")
-        logger.info(f'Success to create CSV file.')
+        logger.info(f"Success to create CSV file.")
         return True
 
     def _get_issues(self):
         # RedmineのIssueフィルターをjsonから取得してIssueを検索
-        issues = self.redmine.filter_issues(**self.data['issue_filter'])
+        issues = self.redmine.issue.filter(**self.data["get_issues_filter"])
 
         # IssueをCSVとして全て出力
-        issues.export('csv', savepath='./output/get_issues.csv', columns='all')
+        issues.export("csv", savepath="./output/get_issues.csv", columns="all")
         return True
+
+    def _check_user_time(self):
+        # RedmineのIssueフィルターをjsonから取得してIssueを検索
+        issues = self.redmine.issue.filter(**self.data["check_time_filter"])
+
+        data_list = []
+        for issue in issues:
+            try:
+                user = issue.assigned_to
+            except ResourceAttrError:
+                user = "担当者なし"
+
+            try:
+                hours = issue.estimated_hours
+            except ResourceAttrError:
+                hours = 0
+
+            data_list.append([user, hours, issue.tracker["name"], issue.subject])
+
+        df = pd.DataFrame(data_list, columns=["user", "estimated_hours", "tracker", "issue"])
+        df.to_csv(f"./output/check_user_time.csv")
+
+        df_sum = df.groupby("user").agg({"estimated_hour": np.sum})
+        df_sum.to_csv(f"./output/check_user_time_summary.csv")
