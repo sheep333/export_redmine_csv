74d8e4d4e8f2a5c780eb8ea78fabaaa19614ef3b
sheep333 <yukiko.elike1.51@gmail.com>
2020-07-23 22:32:39 +0900

    Redmineモジュールから余分な処理を削除

--- redmine.py
+++ redmine.py
@@ -1,20 +1,10 @@
-from redminelib import Redmine
-
-
 class RedmineModule():
     params = ["project_id", "subject", "tracker_id", "description", "status_id", "priority_id",
               "category_id", "fixed_version_id", "is_private", "assigned_to_id", "watcher_user_ids",
               "parent_issue_id", "start_date", "due_date", "estimated_hours", "done_ratio", "custom_fields", "uploads"]
 
-    def __init__(self, **kwargs):
-        # urlとkeyは必須
-        self.redmine = Redmine(**kwargs)
-
-    def get_project(self, project_id):
-        return self.redmine.project.get(project_id)
-
-    def get_issues(self, project_id):
-        return self.redmine.issue.filter(project_id=project_id)
-
-    def filter_issues(self, **kwargs):
-        return self.redmine.issue.filter(**kwargs)
+    @classmethod
+    def param_checker(cls, **kwargs):
+        for key in kwargs:
+            if key not in cls.params:
+                raise ValueError("存在しないパラメーターです。")
