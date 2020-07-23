6841e6ea72443d65b26dc42cd99fc5ff2bfc1b71
sheep333 <yukiko.elike1.51@gmail.com>
2020-07-23 22:31:10 +0900

    pythonをコマンドラインから実行できるように修正

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import logging
 import sys
 
-from .command import Command
+from command import Command
 
 logger = logging.getLogger(__name__)
 
 
 def main():
     # コマンドラインから引数を確認
+    logger.info(f"{sys.argv[1]}コマンドを実行")
     Command.execute_command(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
